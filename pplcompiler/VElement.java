f425d97030989bd43e6f149af7310d8047724be2
jawscout <jake.wilkinson@grandview.edu>
2012-12-05 10:02:28 -0600

    added comments

--- pplcompiler/VElement.java
+++ pplcompiler/VElement.java
@@ -1,5 +1,9 @@
 package pplcompiler;
-
+/**
+ * This is a base type for the elements of the TripV language.
+ * @author Jake Wilkinson
+ *
+ */
 public abstract class VElement{
 	public abstract String toHTML();
 	public String alignment = "top left";
