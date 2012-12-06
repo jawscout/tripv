e39abcd657afd75fe46406d1e922dab0623b5153
godbey <agodbey7@yahoo.com>
2012-12-06 15:28:56 -0600

    Update pplcompiler/SyntaxException.java
    NO CLUE IF THIS WORKS. SOMEONE SHOULD PROB TEST IMO.

---
+++ pplcompiler/SyntaxException.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pplcompiler;
+
+/**
+ *
+ * @author agodbey7
+ */
+public class SyntaxException extends Exception{
+    String reason = null;
+    SyntaxException() {
+        reason = "No reason given";
+    }
+    SyntaxException(String string) {
+        reason = string;
+    }
+
+    @Override
+    public void printStackTrace(){
+        System.out.println(reason);
+        printStackTrace();
+    }
+}
