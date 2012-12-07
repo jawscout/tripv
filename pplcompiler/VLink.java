2601b027d46427f1d6369ab96a36c0034e1755f1
godbey <agodbey7@yahoo.com>
2012-12-06 22:24:52 -0600

    Update pplcompiler/VLink.java
    did some debugging and stuff to make it work with main.

--- pplcompiler/VLink.java
+++ pplcompiler/VLink.java
@@ -6,35 +6,35 @@ class VLink extends VElement
 {
     private String url;
     private String text;
-    
-    public VLink(String[] st, int pos) throws SyntaxException 
+
+    public VLink(String[] st, int pos) throws SyntaxException
     {
         /**
-             * I assume that for all class variables, they are defined in the 
-             * format Parameter = XXXXYYYY. Elements not in this format will 
-             * throw an exception. 
-             */
-        while(pos <= st.length)
+* I assume that for all class variables, they are defined in the
+* format Parameter = XXXXYYYY. Elements not in this format will
+* throw an exception.
+*/
+        while(pos <= st.length-2)
         {
             if(st[pos].equalsIgnoreCase("url"))
             {
                 url = st[pos+2];
-                pos += 2;
+                pos += 3;
             }
             else if(st[pos].equalsIgnoreCase("alignment"))
             {
-                alignment = st[pos+=2];
-                pos += 2;
+                alignment = st[pos+2].substring(1, st[pos+2].length()-1);
+                pos += 3;
             }
             else if(st[pos].equalsIgnoreCase("color"))
             {
                 color = st[pos+2];
-                pos += 2;
+                pos += 3;
             }
             else if(st[pos].equalsIgnoreCase("text"))
             {
                 text = st[pos+2];
-                pos += 2;
+                pos += 3;
             }
             else
             {
@@ -44,11 +44,10 @@ class VLink extends VElement
     }
 
     @Override
-    public String toHTML() 
+    public String toHTML()
     {
-        return "<a href=\""+url+"\">\""+text+"\"</a> ";		
+        return "<a href=\""+url+"\">\""+text+"\"</a> ";
     }
-	
-	
-}
 
+
+}
